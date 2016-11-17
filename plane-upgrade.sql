54d55c86719ab81acd4bdb738a7a0881fcf0bf25
Justin Piper <jpiper4198@gmail.com>
2016-11-17 02:58:38 -0500

    Some code to fix this afternoon

--- plane-upgrade.sql
+++ plane-upgrade.sql
@@ -9,20 +9,43 @@ create or replace function get_next_plane (currPlane in int) return char
 		order by plane_capacity
 		fetch first row only;
 	
+		
 		return nextPlane;
 		-- if nextPlane is null, throw exception
 	end;
 	/
 show errors;
 
-create or replace procedure change_plane_type (currPlane in plane.plane_type%TYPE, flightNo in flight.flight_number%TYPE)
+create or replace function get_next_plane_owner (currPlane in int) return varchar2
+	as
+	owner varchar2(5);
+	
+	begin
+		select owner_id into owner
+		from plane
+		where plane_capacity > currPlane
+		order by plane_capacity
+		fetch first row only;
+		
+		return owner;
+	end;
+	/
+show errors;
+create or replace procedure change_plane_type (currPlane in plane.plane_capacity%TYPE, flightNo in flight.flight_number%TYPE)
 	as
 	
 	nxtPlane char(4) := get_next_plane(currPlane);
-	
+	nxtAirline varchar2(5) := get_next_plane_owner(currPlane);
 	begin
+		set transaction read write name 'update_plane_for_flight';
+		-- set constraints all deferred;
+		
 		update flight
 		set plane_type = nxtPlane
+		where flight_number = flightNo;
+		
+		update flight
+		set airline_id = nxtAirline
 		where flight_number = flightNo;
 		
 		commit;
