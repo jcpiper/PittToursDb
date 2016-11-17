e1426a35883095ed6eb76274c0de224947700834
Justin Piper <jpiper4198@gmail.com>
2016-11-17 10:35:15 -0500

    Procedure and functions are working properly, must set constraints to deffered in trigger!

--- plane-upgrade.sql
+++ plane-upgrade.sql
@@ -1,15 +1,25 @@
 -- Trigger 2
-create or replace function get_next_plane (currPlane in int) return char
+create or replace function get_next_plane (currPlane in int, currType in char) return char
 	as
 	nextPlane char(4);
+	cap int;
 	begin
-		select plane_type into nextPlane
-		from plane
-		where plane_capacity > currPlane
-		order by plane_capacity
-		fetch first row only;
-	
-		
+		select max(plane_capacity) into cap
+		from plane;
+		if currPlane = cap then
+			return currType;
+		else
+			select plane_type into nextPlane
+			from plane
+			where plane_capacity > currPlane
+			order by plane_capacity
+			fetch first row only;
+		end if;
+		-- if nextPlane is null then --current plane is the largest plane
+			-- select plane_type into nextPlane
+			-- from plane
+			-- where plane_capacity = currPlane and plane_type = currType;
+		-- end if;
 		return nextPlane;
 		-- if nextPlane is null, throw exception
 	end;
@@ -31,23 +41,25 @@ create or replace function get_next_plane_owner (currPlane in int) return varcha
 	end;
 	/
 show errors;
-create or replace procedure change_plane_type (currPlane in plane.plane_capacity%TYPE, flightNo in flight.flight_number%TYPE)
+-- procedure to change the plane for a flight
+-- must set constraints to deffered before hand!
+create or replace procedure change_plane_type (currPlane in plane.plane_capacity%TYPE, currType in plane.plane_type%TYPE, flightNo in flight.flight_number%TYPE)
 	as
-	
-	nxtPlane char(4) := get_next_plane(currPlane);
-	nxtAirline varchar2(5) := get_next_plane_owner(currPlane);
+	nxtPlane char(4) := get_next_plane(currPlane, currType);
+	nxtAirline varchar2(5);
 	begin
-		set transaction read write name 'update_plane_for_flight';
-		-- set constraints all deferred;
-		
-		update flight
-		set plane_type = nxtPlane
-		where flight_number = flightNo;
-		
-		update flight
-		set airline_id = nxtAirline
-		where flight_number = flightNo;
-		
+		if nxtPlane = currType then --largest plane
+			commit;
+		else
+			nxtAirline := get_next_plane_owner(currPlane);
+			update flight
+			set plane_type = nxtPlane
+			where flight_number = flightNo;
+			
+			update flight
+			set airline_id = nxtAirline
+			where flight_number = flightNo;
+		end if;
 		commit;
 	end;
 	/
