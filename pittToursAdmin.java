ac1c7964526bbb356066dbb5899981d786bf81c7
Justin Piper <jpiper4198@gmail.com>
2016-11-25 13:48:53 -0500

    Added methods for all admin options, need to build stored procedures

--- pittToursAdmin.java
+++ pittToursAdmin.java
@@ -24,18 +24,23 @@ public class pittToursAdmin {
 				break;
 			case "2":
 				System.out.println("You have chosen to load airline info.");
+				loadAirline();
 				break;
 			case "3":
 				System.out.println("You have chosen to load schedule info.");
+				loadSchedule();
 				break;
 			case "4":
 				System.out.println("You have chosen to load pricing info.");
+				loadPricing();
 				break;
 			case "5":
 				System.out.println("You have chosen to load plane info.");
+				loadPlane();
 				break;
 			case "6":
 				System.out.println("You have chosen to generate passenger manifest for specific flight on given day.");
+				generatePassengerList();
 				break;
 			default:
 				System.out.println("Invalid option, choose again");
@@ -59,5 +64,51 @@ public class pittToursAdmin {
 			main(args);
 		}
 	}
-	// add methods for the remaining options
+	public static void loadAirline() throws IOException {
+		System.out.println("Preparing to load airline information.");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the file name where the airline info is stored.");
+		String file = input.readLine();
+		// load data from airline file into airline table
+	}
+	
+	public static void loadSchedule() throws IOException {
+		System.out.println("Preparing to load schedule information");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the file name where the schedule info is stored.");
+		String file = input.readLine();
+		// load data from schedule file into flight table of db
+	}
+	
+	public static void loadPricing() throws IOException {
+		System.out.println("Preparing to load pricing information.");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the file name where the pricing info is stored.");
+		String file = input.readLine();
+		// load data from schedule file into price table of db
+	}
+	
+	public static void loadPlane() throws IOException {
+		System.out.println("Preparing to load plane information.");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the file name where the plane info is stored.");
+		String file = input.readLine();
+		// load data from schedule file into plane table of db
+	}
+	
+	public static void generatePassengerList() throws IOException {
+		System.out.println("Preparing to generate passenger manifest.");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter the flight number of the desired flight.");
+		String flightNum = input.readLine();
+		System.out.println("Please enter the desired date.");
+		String flightDate = input.readLine();
+		// call procedure and print results
+		/*
+			select customer info from join of customer, reservation, res_detail
+			select c.salutation, c. firstName, c.lastName
+			from Customer c, Reservation r, Reservation_Detail d
+			where r. reservation_number = d.reservation_number and c.cid = r.cid and d.flight_number = flightNum and d.date = flightDate;
+		*/
+	}
 }
