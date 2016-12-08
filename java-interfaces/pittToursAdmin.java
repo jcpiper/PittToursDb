64e23e284c5d10b7d8e89580a7f96fb989d2b327
Justin Piper <jpiper4198@gmail.com>
2016-12-08 04:15:09 -0500

    EraseDb procedure successfully called from java!

--- java-interfaces/pittToursAdmin.java
+++ java-interfaces/pittToursAdmin.java
@@ -2,10 +2,49 @@
 * Administrator interface for pittTours
 */
 import java.io.*;
+import java.sql.*;
 
 public class pittToursAdmin {
 	
 	public static void main (String[] args) throws IOException {
+		
+		// set up db connection
+		Connection conn = null;
+		try {
+			// register oracle driver
+			DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
+			
+			String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass";
+			String uname = "jcp68";
+			String passwrd = "3788990";
+			conn = DriverManager.getConnection(url, uname, passwrd);
+			
+		} catch (SQLException e) {
+
+			System.out.println("Where is your Oracle JDBC Driver?");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("Oracle JDBC Driver Registered!");
+
+		// Statement stmt = null;
+		// String query = "Select * from flight";
+		
+		// try {
+			// stmt = conn.createStatement();
+			// ResultSet rs = stmt.executeQuery(query);
+			
+			// System.out.println("Query made!");
+			
+			// stmt.close();
+		// } catch (SQLException e) {
+			// System.out.println("Query failed!");
+			// e.printStackTrace();
+			// return;
+		// } 
+		
 		System.out.println("Welcome! Please select an option to proceed.");
 		System.out.println("1. Erase database.");
 		System.out.println("2. Load airline info.");
@@ -20,28 +59,28 @@ public class pittToursAdmin {
 	
 		if (option.equals("1")) {
 			System.out.println("You have chosen to erase the database.");
-			eraseDb();
+			eraseDb(conn);
 		}
 			
 		else if (option.equals("2")) {
 			System.out.println("You have chosen to load airline info.");
-			loadAirline();
+			loadAirline(conn);
 		}
 		else if (option.equals("3")) {
 			System.out.println("You have chosen to load schedule info.");
-			loadSchedule();
+			loadSchedule(conn);
 		}
 		else if (option.equals("4")) {
 			System.out.println("You have chosen to load pricing info.");
-			loadPricing();
+			loadPricing(conn);
 		}
 		else if (option.equals("5")) {
 			System.out.println("You have chosen to load plane info.");
-			loadPlane();
+			loadPlane(conn);
 		}
 		else if (option.equals("6")) {
 			System.out.println("You have chosen to generate passenger manifest for specific flight on given day.");
-			generatePassengerList();
+			generatePassengerList(conn);
 		}
 		else {
 			System.out.println("Invalid option, choose again");
@@ -50,14 +89,25 @@ public class pittToursAdmin {
 		}
 	}
 	
-	public static void eraseDb() throws IOException {
+	public static void eraseDb(Connection conn) throws IOException {
 		System.out.println("Are you sure you want to erase the database?\ny/n");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		String confirm = input.readLine();
+		String confirm = input.readLine().toLowerCase();
 		
 		if (confirm.equals("y")) {
-			System.out.println("Erasing the database.");
-			//call eraseDb procedure from pl/sql
+			try {
+				System.out.println("Erasing the database.");
+				//call eraseDb procedure from pl/sql
+				// Statement stmt = conn.createStatement();
+				String eraseData = "{call eraseDb}";
+				CallableStatement stmt = conn.prepareCall(eraseData);
+				stmt.executeUpdate();
+				stmt.close();
+			} catch (SQLException e) {
+				System.out.println("Call to procedure failed!");
+				e.printStackTrace();
+				return;
+			}
 		}
 		else {
 			System.out.println("Action aborted, returning to main menu\n");
@@ -65,7 +115,7 @@ public class pittToursAdmin {
 			main(args);
 		}
 	}
-	public static void loadAirline() throws IOException {
+	public static void loadAirline(Connection conn) throws IOException {
 		System.out.println("\n\nPreparing to load airline information...");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("\nPlease enter the file name where the airline info is stored.");
@@ -99,7 +149,7 @@ public class pittToursAdmin {
 		csv.close();
 	}
 	
-	public static void loadSchedule() throws IOException {
+	public static void loadSchedule(Connection conn) throws IOException {
 		System.out.println("\n\nPreparing to load schedule information...");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please enter the file name where the schedule info is stored.\n");
@@ -127,7 +177,7 @@ public class pittToursAdmin {
 		}		
 	}
 	
-	public static void loadPricing() throws IOException {
+	public static void loadPricing(Connection conn) throws IOException {
 		System.out.println("Preparing to load pricing information.");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("\nWould you like to LOAD PRICING INFORMATION or CHANGE THE PRICE OF AN EXISTING FLIGHT?");
@@ -136,18 +186,18 @@ public class pittToursAdmin {
 		String choice = input.readLine().toUpperCase();
 		if (choice.equals("L")) {
 			// input.close();
-			loadPriceData();
+			loadPriceData(conn);
 		}
 		else if(choice.equals("C")) {
 			// input.close();
-			changePriceData();
+			changePriceData(conn);
 		}
 		else {
 			System.out.println("ERROR: Invalid input! Options are \"L\" or \"C\"!");
-			loadPricing();
+			loadPricing(conn);
 		}			
 	}
-	public static void changePriceData() throws IOException {
+	public static void changePriceData(Connection conn) throws IOException {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("\nPlease enter the departure city for the flight. E.G. \"CHI\"");
 		System.out.println("Waiting for input...");
@@ -183,7 +233,7 @@ public class pittToursAdmin {
 		input.close();
 		// call changePrice with params depCity, arrCity, hiPrice, loPrice
 	}
-	public static void loadPriceData() throws IOException {
+	public static void loadPriceData(Connection conn) throws IOException {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("\nPlease enter the file name where the pricing info is stored.");
 		System.out.println("Waiting for input...");
@@ -211,7 +261,7 @@ public class pittToursAdmin {
 		}
 	}
 	
-	public static void loadPlane() throws IOException {
+	public static void loadPlane(Connection conn) throws IOException {
 		System.out.println("Preparing to load plane information.");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please enter the file name where the plane info is stored.");
@@ -240,7 +290,7 @@ public class pittToursAdmin {
 		}
 	}
 	
-	public static void generatePassengerList() throws IOException {
+	public static void generatePassengerList(Connection conn) throws IOException {
 		System.out.println("Preparing to generate passenger manifest.");
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please enter the flight number of the desired flight.");
